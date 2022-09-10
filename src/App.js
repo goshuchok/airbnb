b161dac87afb551236404a5055db7a80b36fb29c
Igor Povar <goshuchok@gmail.com>
2022-09-10 10:56:54 +0300

    add carousel card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Box, CssBaseline } from '@mui/material';
+import { Box, CssBaseline, Container } from '@mui/material';
 import './App.css';
 import Header from './components/Header';
 import OptionsTab from './components/OptionsTab';
+import LocationCards from './components/LocationCards';
 
 function App() {
   return (
@@ -12,6 +13,19 @@ function App() {
           <Box>
             <Header />
             <OptionsTab />
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              flexGrow: 1,
+              height: 100,
+              overflowY: 'scroll',
+            }}
+          >
+            <Container maxWidth="xl" sx={{ mb: 3 }}>
+              <LocationCards />
+            </Container>
           </Box>
         </Box>
       </CssBaseline>
