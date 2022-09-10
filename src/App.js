4edd5b6d73b5190ecedb8a3763629172d6adef5c
Igor Povar <goshuchok@gmail.com>
2022-09-10 11:54:25 +0300

    add footer fom mobile screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Box, CssBaseline, Container } from '@mui/material';
 import './App.css';
 import Header from './components/Header';
 import OptionsTab from './components/OptionsTab';
 import LocationCards from './components/LocationCards';
 import Footer from './components/Footer';
+import FooterMenu from './components/FooterMenu';
+import { displayOnDesktop } from './themes/commonStyles';
+import MobileFooter from './components/MobileFooter';
 
 function App() {
   return (
-    <React.Fragment>
+    <Fragment>
       <CssBaseline>
         <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
           <Box>
@@ -26,14 +29,20 @@ function App() {
           >
             <Container maxWidth="xl" sx={{ mb: 3 }}>
               <LocationCards />
+              <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                <MobileFooter />
+              </Box>
             </Container>
           </Box>
-          <Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <FooterMenu />
+          </Box>
+          <Box sx={displayOnDesktop}>
             <Footer />
           </Box>
         </Box>
       </CssBaseline>
-    </React.Fragment>
+    </Fragment>
   );
 }
 
